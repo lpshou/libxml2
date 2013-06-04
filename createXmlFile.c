4c4562d1dd9af7454f56888762078fd1c18975c9
lpshou <lp_shou@163.com>
2013-06-04 11:13:41 +0800

    Update createXmlFile.c

--- createXmlFile.c
+++ createXmlFile.c
@@ -2,61 +2,67 @@
 #include<string.h>
 #include<libxml/parser.h>
 #include<libxml/tree.h>
+#define NumOfBucket 3
+#define NumOfObj 4
 void createXmlFile(xmlChar* rootName);
-/*void main()
+void main()
 {
-        xmlChar * str =BAD_CAST"root4";
-        createXmlFile(str);
-}*/
+    xmlChar * str =BAD_CAST"client1";
+    createXmlFile(str);
+}
 void createXmlFile(xmlChar* rootName)
 {
 
-        xmlDocPtr doc = xmlNewDoc(BAD_CAST"1.0");
-        xmlNodePtr root_node = xmlNewNode(NULL,(xmlChar*)rootName);
+    xmlDocPtr doc = xmlNewDoc(BAD_CAST"1.0");
+    xmlNodePtr root_node = xmlNewNode(NULL,(xmlChar*)rootName);
 
-        //设置根节点
-        xmlDocSetRootElement(doc,root_node);
+    //设置根节点
+    xmlDocSetRootElement(doc,root_node);
 
-        //直接在根节点中创建节点
-        xmlNewTextChild(root_node,NULL,BAD_CAST"newNode1",BAD_CAST"new node1 content");
-        xmlNewTextChild(root_node,NULL,BAD_CAST"newNode2",BAD_CAST"new node2 content");
-        xmlNewTextChild(root_node,NULL,BAD_CAST"newNode3",BAD_CAST"new node3 content");
-
-        //创建一个节点，设置其内容和属性，然后加入根节点
-        xmlNodePtr node = xmlNewNode(NULL,BAD_CAST"client1");
-        xmlNodePtr content = xmlNewText(BAD_CAST"client content");
-
-        xmlAddChild(root_node,node);
-        xmlAddChild(node,content);
-        xmlNewProp(node,BAD_CAST"attribute",BAD_CAST"yes");
-
-        xmlNodePtr node1 = xmlNewNode(NULL,BAD_CAST"bucket1");
-        xmlAddChild(node,node1);
-        xmlNodePtr node2 = xmlNewNode(NULL,BAD_CAST"bucket2");
-        xmlAddChild(node,node2);
-
-        xmlNodePtr node12 = xmlNewNode(NULL,BAD_CAST"obj1");
-        xmlAddChild(node1,node12);
-        xmlNodePtr node13 = xmlNewNode(NULL,BAD_CAST"obj2");
-        xmlAddChild(node1,node13);
-
-
-        //创建 一个儿子和孙子节点
-        node = xmlNewNode(NULL,BAD_CAST"son");
-        xmlAddChild(root_node,node);
-
-        xmlNodePtr grandson = xmlNewNode(NULL,BAD_CAST"grandson");
-        xmlAddChild(node,grandson);
-        xmlAddChild(grandson,xmlNewText(BAD_CAST"this is a grandson node"));
-
-        //存储xml文档
-        xmlChar str[50];
-        strcpy(str,rootName);
-        strcat(str,".xml");
-        int nRel = xmlSaveFile(str,doc);
-        if(nRel!=-1)
+    //直接在根节点中创建节点
+    int i =0;
+    int j=0;
+    xmlNodePtr node,nodeObj;
+    for (i=0;i<NumOfBucket;++i)
+    {
+        char temp[2];
+        char tmp[50];
+        sprintf(temp,"%d",i+1);
+        strcpy(tmp,"buc");
+        strcat(tmp,temp);
+        node = xmlNewTextChild(root_node,NULL,BAD_CAST(tmp),NULL);
+        for (j=0;j<NumOfObj;++j)
         {
-                printf("%s is create size is:%d\n",(char *)str,nRel);
+            char objtmp[50];
+            char objtemp[2];
+            sprintf(objtemp,"%d",j+1);
+            strcpy(objtmp,"obj");
+            strcat(objtmp,objtemp);
+            xmlNewTextChild(node,NULL,BAD_CAST(objtmp),NULL);
         }
-        xmlFreeDoc(doc);
+
+    }
+
+    //创建一个节点，设置其内容和属性，然后加入根节点
+    /* xmlNodePtr node = xmlNewNode(NULL,BAD_CAST"buc4");
+     xmlAddChild(root_node,node);
+
+     xmlNodePtr node1 = xmlNewNode(NULL,BAD_CAST"obj1");
+     xmlAddChild(node,node1);
+     xmlNodePtr node2 = xmlNewNode(NULL,BAD_CAST"obj2");
+     xmlAddChild(node,node2);
+     xmlNodePtr node3 = xmlNewNode(NULL,BAD_CAST"obj3");
+     xmlAddChild(node,node3);*/
+
+
+    //存储xml文档
+    xmlChar str[50];
+    strcpy(str,rootName);
+    strcat(str,".xml");
+    int nRel = xmlSaveFile(str,doc);
+    if (nRel!=-1)
+    {
+        printf("%s is create size is:%d\n",(char *)str,nRel);
+    }
+    xmlFreeDoc(doc);
 }
