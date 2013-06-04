0a1ef7e9e0cb5024fa4c0526ff8be07334fd38b3
lpshou <lp_shou@163.com>
2013-06-04 15:13:14 +0800

    Update tt.c

--- tt.c
+++ tt.c
@@ -1,4 +1,3 @@
-
 /*
 *compiler command:
 *gcc -I/usr/local/include/ -L/usr/local/lib -ltokyocabinet -ltokyotyrant -o *tt tt.c
@@ -6,11 +5,36 @@
 
 #include<tcrdb.h>
 #include<stdlib.h>
-#include<stdbool.h>
-#include<stdint.h>
+char *GetValue(char *key)
+{
+        TCRDB*rdb;
+        char *value;
+        rdb = tcrdbnew();
+        if(!tcrdbopen(rdb,"192.168.0.151",1987))
+        {
+                printf("cannot open \n");
+                exit(-1);
+        }
+        value = tcrdbget2(rdb,key);
+        if(value)
+        {
+                printf("the value of key %s is:%s\n",key,value);
+                return value;
+        }
+        else
+        {
+                printf("key %s doesnot exists\n",key);
+                return NULL;
+        }
+        tcrdbclose(rdb);
+}
+
 int main(int argc,char *argv[])
 {
-        TCRDB *rdb;
+
+        char *value = GetValue("foo");
+        printf("the value  is %s\n",value);
+       /* TCRDB *rdb;
         int ecode;
         char *value;
 
@@ -36,6 +60,6 @@ int main(int argc,char *argv[])
                 printf("cannot get\n");
         }
 
-        tcrdbclose(rdb);
+        tcrdbclose(rdb);*/
         return 0;
 }
